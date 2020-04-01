8f2e49a4ba892f4e0138868464b692a75929b207
aniawyroba <aniawyroba@gmail.com>
2020-04-01 15:42:40 +0200

    installed slug, adjusted Recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slug = require('slug');
 
 const RecipeSchema = new mongoose.Schema({
   slug: {
@@ -8,7 +9,23 @@ const RecipeSchema = new mongoose.Schema({
   },
   title: {
     type: String,
+  },
+  description: {
+    type: String,
+  },
+});
+
+RecipeSchema.pre('save', async function (next) {
+  this.slug = slug(this.title);
+
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const issuesWithSulg = await this.constructor.find({ slug: slugRegEx });
+
+  if (issuesWithSulg.length) {
+    this.slug = `${this.slug}-${issuesWithSulg.length + 1}`;
   }
+
+  next();
 });
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
