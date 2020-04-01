fd7643ff6ecb31cae438311a76cc87f5ff487b4d
aniawyroba <aniawyroba@gmail.com>
2020-04-01 15:01:24 +0200

    installed dotenv, created .env file and added App's port

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 // Configs
 const app = express();
 
-app.set('port', 3000);
+// DotEnv Config
+dotenv.config();
+
+// Set App port
+app.set('port', process.env.PORT || 3000);
+
+// Start App
 app.listen(app.get('port'), () => {
-  console.log('Listening on port 3000');
+  console.log(`Listening on port: ${app.get('port')}`);
 });
